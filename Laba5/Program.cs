48a5a8707ac4f6ab03220cd2b9ded3383643f105
khomenkovetal <khomenkovetal@gmail.com>
2021-05-25 13:31:38 +0300

    Добавьте файлы проекта.

---
+++ Laba5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laba5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
